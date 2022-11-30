09b8ad51bd7163b92fd24491655b05c9315ddde5
Yunusa David <daveed856@outlook.com>
2022-11-30 11:19:52 +0100

    change backend hosting link

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://tinder-clone-backend-production.up.railway.app/',
+  baseURL: 'https://fragile-tan-sunglasses.cyclic.app/',
 });
